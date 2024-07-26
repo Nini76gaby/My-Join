b99ea3385fd8d064af6ed1a4619e15c2e6d3f990
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2024-07-26 16:36:51 +0200

    html and css

---
+++ js/login.js
@@ -0,0 +1,20 @@
+
+
+function openLoginPage() {
+    window.location.href = "login.html";
+}
+
+
+function openSignUpPage() {
+    window.location.href = "signup.html";
+}
+
+
+function openPrivacyPolicyPage() {
+    window.location.href = "privacypolicy.html";
+}
+
+
+function openLegalNoticePage() {
+    window.location.href = "legalnotice.html";
+}
